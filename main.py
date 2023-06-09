0317e9e8b0212598fd08dae7ea77b3bf00177b50
antoi <antoine.rovini@epitech.eu>
2023-06-09 16:41:02 +0200

    There was a bug in the system. Debugged it and restored functionality.

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from format_json import *
 from json_to_csv import *
 from datetime import datetime
 
-if "__name__" == "__main__":
-    json_file = f'{datetime.now()}_flights.json'
-    csv_file = f'tab_{json_file}'
-    scrap_plane(json_file)
-    format_json(json_file, json_file)
-    json_to_csv(json_file, csv_file)
-    print("all operations done")
-
+current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+namefile = f'{current_time}_flight'
+json_file = f'{namefile}.json'
+csv_file = f'tab_{namefile}.csv'
+with open(json_file, 'w') as file:
+    pass
+with open(csv_file, 'w') as file:
+    pass
+scrap_plane(json_file)
+format_json(json_file, json_file)
+json_to_csv(json_file, csv_file)
+print("all operations done")

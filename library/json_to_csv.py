9142fd486b7312457a80f5fbcc1399a8025a1df0
antoi <antoine.rovini@epitech.eu>
2023-06-14 10:53:00 +0200

    modif functionnal, dumping on the git

--- library/json_to_csv.py
+++ library/json_to_csv.py
@@ -5,13 +5,24 @@ import json
 def json_to_csv(file_in, file_out):
     with open(file_in) as file:
         data = json.load(file)
+
     response_data = data['response']
-    parameters = []
-    for response_item in response_data:
-        parameters.append(list(response_item.values()))
-    param_names = list(response_data[0].keys())
-    parameters.insert(0, param_names)
+    param_names = [
+        'hex', 'reg_number', 'flag', 'lat', 'lng', 'alt', 'dir', 'speed',
+        'v_speed', 'squawk', 'flight_number', 'flight_icao', 'flight_iata',
+        'dep_icao', 'dep_iata', 'arr_icao', 'arr_iata', 'airline_icao',
+        'airline_iata', 'aircraft_icao', 'updated', 'status'
+    ]
+
     with open(file_out, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerows(parameters)
-    print(f'Data converted in {file_out}')
+        writer.writerow(param_names)
+
+        for response_item in response_data:
+            row = []
+            for param_name in param_names:
+                param_value = response_item.get(param_name, '')
+                row.append(param_value)
+            writer.writerow(row)
+
+    print(f'Data converted to {file_out}')

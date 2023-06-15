3bfac8f0b9fe8270bc9c1fbf66cc84a9cce53e91
antoi <antoine.rovini@epitech.eu>
2023-06-15 10:49:36 +0200

    modif

--- library/coordinates.py
+++ library/coordinates.py
@@ -1,7 +1,6 @@
 def read_coordinates(file_path):
     latitudes = []
     longitudes = []
-
     with open(file_path, 'r') as file:
         for line in file:
             if line.startswith('lat'):
@@ -10,6 +9,4 @@ def read_coordinates(file_path):
             elif line.startswith('lng'):
                 lng_values = line.split(':')[1].strip().split()
                 longitudes.extend(lng_values)
-
     return latitudes, longitudes
-    # Afficher les valeurs des latitudes et longitudes
--- list_planes.py
+++ list_planes.py
@@ -3,17 +3,23 @@ from library.coordinates import *
 
 filename = input('Entrez le nom du fichier CSV entier (incluant l\'extension du fichier): ')
 coord_path = 'coordinates.txt'
-
 latitudes, longitudes = read_coordinates(coord_path)
-
 data = read_csv(filename)
 n = 0
+new_filename = 'sorted_' + filename
+
+with open(new_filename, 'w') as file :
+    pass
+
+print('calcul du nombre d\'avions dans une zone précise.')
 for row in data:
-    print('calcul du nombre d\'avions dans une zone précise')
-    n = n + 1
-    print(f'#{n}\tLat:\t', row['lat'], '\tLng:\t', row['lng'])
-
-
+    lat = row['lat']
+    lng = row['lng']
+    if float(latitudes[0]) <= float(lat) <= float(latitudes[1]) \
+            or float(latitudes[0]) >= float(lat) >= float(latitudes[1]):
+        n = n + 1
+        print(f'plane number #{n}\tLat: {lat}\tLng: {lng}')
+    # pas opti: print(f'#{n}\tLat:\t', row['lat'], '\tLng:\t', row['lng'])
 
 print(latitudes)
 print(longitudes)

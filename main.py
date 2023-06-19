9a4ca431c2483ba32425ac45f66b7d5aee8c5a6e
antoi <antoine.rovini@epitech.eu>
2023-06-19 14:15:52 +0200

    automatisation done

--- main.py
+++ main.py
@@ -5,19 +5,25 @@ from library.format_json import *
 from library.json_to_csv import *
 from datetime import datetime
 
+
+def get_data(n):
+    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    namefile = f'{current_time}_flight'
+    json_file = f'{namefile}.json'
+    csv_file = f'tab_{namefile}.csv'
+    with open(json_file, 'w') as file:
+        pass
+    with open(csv_file, 'w') as file:
+        pass
+    scrap_plane(json_file)
+    format_json(json_file, json_file)
+    json_to_csv(json_file, csv_file)
+    print("all operations done")
+    if n == 0:
+        print('waiting an hour to proceed further.')
+        sleep(3600)
+        get_data(n)
+
+
 n = get_user_choice()
-
-
-current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-namefile = f'{current_time}_flight'
-json_file = f'{namefile}.json'
-csv_file = f'tab_{namefile}.csv'
-with open(json_file, 'w') as file:
-    pass
-with open(csv_file, 'w') as file:
-    pass
-scrap_plane(json_file)
-format_json(json_file, json_file)
-json_to_csv(json_file, csv_file)
-print("all operations done")
-sleep(3600)
+get_data(n)

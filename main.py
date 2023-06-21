c98a5d329658a05b7b018ac9d781e93cbe3598b2
antoi <antoine.rovini@epitech.eu>
2023-06-21 09:49:46 +0200

    change the dinamyc of the library

--- main.py
+++ main.py
@@ -1,34 +1,5 @@
-from time import sleep
 from library.user_define import *
-from library.flight_scrapper import *
-from library.format_json import *
-from library.json_to_csv import *
-from library.json_to_excel import *
-from datetime import datetime
-
-
-def get_data(n):
-    current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    namefile = f'{current_time}_flight'
-    json_file = f'{namefile}.json'
-    csv_file = f'tab_{namefile}.csv'
-    excel_file = f'excel_{namefile}.xls'
-    with open(json_file, 'w') as file:
-        pass
-    with open(csv_file, 'w') as file:
-        pass
-    with open(excel_file, 'w') as file:
-        pass
-    scrap_plane(json_file)
-    format_json(json_file, json_file)
-    json_to_csv(json_file, csv_file)
-    json_to_excel(json_file, excel_file)
-    print("all operations done")
-    if n == 0:
-        print('waiting an hour to proceed further.')
-        sleep(3600)
-        get_data(n)
-
+from library.data_retriever import *
 
 n = get_user_choice()
 get_data(n)

ff100c84131338f4b79698768542f69a5d38ef98
antoi <antoine.rovini@epitech.eu>
2023-07-12 14:08:40 +0200

    add a package

---
+++ library/install.c
@@ -0,0 +1,17 @@
+//
+// Created by antoi on 12/07/2023.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("pip install pandas");
+    system("pip install xlwt");
+    system("pip install xlsxwriter");
+    system("pip install xls");
+    system("pip install openpyxl");
+    system("pip install row");
+    system("pip install numpy");
+}

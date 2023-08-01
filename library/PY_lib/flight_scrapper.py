eb48b4161bb43e8b7c87eb4a680384b38c1956ca
antoi <antoine.rovini@epitech.eu>
2023-08-01 16:53:00 +0200

    cleaning the file

--- library/PY_lib/flight_scrapper.py
+++ library/PY_lib/flight_scrapper.py
@@ -3,7 +3,7 @@ import json
 
 
 def scrap_plane(input_json):
-    api_key = 'c78bbb29-56b2-4347-9d89-fd3e72238896'
+    api_key = '361444b0-bbd6-4da0-a1ce-85a76197bd4f'
     url = f"https://airlabs.co/api/v9/flights?lat=0&lon=0&distance=10&api_key={api_key}"
     response = requests.get(url)
     if response.status_code == 200:
